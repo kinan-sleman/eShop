92376a6534a8180bc5cafe18bf1a455565d53719
Kinan Sleman <kenan@oonatech.com>
2025-05-30 11:30:36 +0300

    fix checkout products cards

--- src/components/Checkout/CheckoutProduct.tsx
+++ src/components/Checkout/CheckoutProduct.tsx
@@ -1,6 +1,7 @@
 import { useStateContext } from "../../contexts/StateContext";
 import type { ProductProps } from "../../types/product";
 import { toast } from 'react-toastify';
+
 export default function CheckoutProduct({ id, title, price, rating, thumbnail }: ProductProps) {
     const [, dispatch] = useStateContext()
     const removeFromBasket = () => {
@@ -25,25 +26,34 @@ export default function CheckoutProduct({ id, title, price, rating, thumbnail }:
             theme: "light",
         });
     }
-    return (
-        <div key={id} className="checkoutProduct">
-            <img src={thumbnail} alt="" className="checkoutProduct__image" />
 
-            <div className="checkoutProduct__info">
-                <p className="checkoutProduct__title">{title}
-                </p>
-                <p className="checkoutProduct__price">
-                    <small>$</small>
-                    <strong>{price}</strong>
-                </p>
-                <div className="checkoutProduct__rating">
-                    {Array.from({ length: rating }, (_, i) => (
-                        <span key={i}>⭐</span>
-                    ))}
+    return (
+        <div className="checkoutProduct">
+            <div className="checkoutProduct__container">
+                <img src={thumbnail} alt="" className="checkoutProduct__image" />
+                <div className="checkoutProduct__info">
+                    <div className="checkoutProduct__title-container">
+                        <p className="checkoutProduct__title">{title}</p>
+                    </div>
+                    <div className="checkoutProduct__price-container">
+                        <p className="checkoutProduct__price">
+                            <small>$</small>
+                            <strong>{price}</strong>
+                        </p>
+                    </div>
+                    <div className="checkoutProduct__rating-container">
+                        <div className="checkoutProduct__rating">
+                            {Array.from({ length: rating }, (_, i) => (
+                                <span key={i}>⭐</span>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="checkoutProduct__button-container">
+                        <button onClick={removeFromBasket} className="checkoutProduct__removeButton">
+                            Remove from Basket
+                        </button>
+                    </div>
                 </div>
-                <button onClick={removeFromBasket} className="checkoutProduct__removeButton">
-                    Remove from Basket
-                </button>
             </div>
         </div>
     )
